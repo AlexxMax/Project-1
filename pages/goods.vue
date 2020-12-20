d364ef8881fd1a6b4829df32f65f16e9a0e3d8d9
AlexxMax <C:\Users\\AppData\Roaming\The Bat!>
2020-12-20 17:31:41 +0200

    vuex added

--- pages/goods.vue
+++ pages/goods.vue
@@ -15,41 +15,11 @@
 import GoodsListItem from '@/components/GoodsListItem'
 export default {
   components: { GoodsListItem },
-  data() {
-    return {
-      list: [
-        {
-          id: 1,
-          name: 'Product - 1',
-          price: 111,
-          description: 'Description - 1',
-        },
-        {
-          id: 2,
-          name: 'Product - 2',
-          price: 222,
-          description: 'Description - 2',
-        },
-        {
-          id: 3,
-          name: 'Product - 3',
-          price: 333,
-          description: 'Description - 3',
-        },
-        {
-          id: 4,
-          name: 'Product - 4',
-          price: 444,
-          description: 'Description - 4',
-        },
-        {
-          id: 5,
-          name: 'Product - 5',
-          price: 555,
-          description: 'Description - 5',
-        },
-      ],
+  computed:{
+    list(){
+      return this.$store.state.list
     }
-  },
-}
+  }
+ 
+  }
 </script>
---
+++ store/index.js
@@ -0,0 +1,34 @@
+export const state = () => ({
+  list: [
+    {
+      id: 1,
+      name: 'Product - 1',
+      price: 111,
+      description: 'Description - 1',
+    },
+    {
+      id: 2,
+      name: 'Product - 2',
+      price: 222,
+      description: 'Description - 2',
+    },
+    {
+      id: 3,
+      name: 'Product - 3',
+      price: 333,
+      description: 'Description - 3',
+    },
+    {
+      id: 4,
+      name: 'Product - 4',
+      price: 444,
+      description: 'Description - 4',
+    },
+    {
+      id: 5,
+      name: 'Product - 5',
+      price: 555,
+      description: 'Description - 5',
+    },
+  ]
+})
